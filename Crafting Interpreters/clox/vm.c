9175fd61a36c7659fe2ada7393550829c999232d
chytonpide <yadameda@naver.com>
2025-09-04 14:40:10 +0900

    Add "Types of Values" chapter

--- Crafting Interpreters/clox/vm.c
+++ Crafting Interpreters/clox/vm.c
@@ -18,7 +18,7 @@ static void resetStack() {
 static void runtimeError(const char* format, ...) {
   va_list args;
   va_start(args, format);
-  vfprint(stderr, format, args);
+  vfprintf(stderr, format, args);
   va_end(args);
   fputs("\n", stderr);
 
@@ -35,14 +35,41 @@ void initVM() {
 void freeVM() {
 }
 
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
+}
+
+static Value peek(int distance) {
+  return vm.stackTop[-1 - distance];
+}
+
+static bool isFalsy(Value value) {
+  // value 가 nil 이면 isFalsy 는 true 다.
+  // value 가 bool 이 아니면, isFalsy 는 false 다.
+  // value 가 bool 이고, true 이면, isFalsy 는 false 다.
+  // value 가 bool 이고, false 이면, isFalsy 는 true 다.
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op) \
+#define BINARY_OP(valueType, op) \
     do { \
-      double b = pop(); \
-      double a = pop(); \
-      push(a op b); \
+      if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+        runtimeError("Operands must be numbers."); \
+        return INTERPRET_RUNTIME_ERROR; \
+      } \
+      double b = AS_NUMBER(pop()); \
+      double a = AS_NUMBER(pop()); \
+      push(valueType(a op b)); \
     } while (false)
 
   for (;;) {
@@ -63,13 +90,27 @@ static InterpretResult run() {
         push(constant);
         break;
       }
-      case OP_ADD:      BINARY_OP(+); break;
-      case OP_SUBTRACT: BINARY_OP(-); break;
-      case OP_MULTIPLY: BINARY_OP(*); break;
-      case OP_DIVIDE:   BINARY_OP(/); break;
+      case OP_NIL:      push(NIL_VAL); break;
+      case OP_TRUE:     push(BOOL_VAL(true)); break;
+      case OP_FALSE:    push(BOOL_VAL(false)); break;
+      case OP_EQUAL:    {
+        Value b = pop();
+        Value a = pop();
+        push(BOOL_VAL(valuesEqual(a,b)));
+        break;
+      }
+      case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
+      case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
+      case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
+      case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+      case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+      case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
+      case OP_NOT:
+        push(BOOL_VAL(isFalsy(pop())));
+        break;
       case OP_NEGATE:
         if (!IS_NUMBER(peek(0))) {
-          runtimeError("Operand must be a number.")
+          runtimeError("Operand must be a number.");
           return INTERPRET_RUNTIME_ERROR;
         }
         push(NUMBER_VAL(-AS_NUMBER(pop())));
@@ -104,18 +145,3 @@ InterpretResult interpret(const char* source) {
   freeChunk(&chunk);
   return result;
 }
-
-void push(Value value) {
-  *vm.stackTop = value;
-  vm.stackTop++;
-}
-
-Value pop() {
-  vm.stackTop--;
-  return *vm.stackTop;
-}
-
-static Value peek(int distance) {
-  return vm.stackTop[-1 - distance];
-}
-
